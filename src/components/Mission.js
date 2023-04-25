a2e0dcf26a2fd150bdbb52b19f1893848f9073c2
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-04-25 19:42:15 +0200

    Select store in mission component

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMissions } from "../redux/mission/missionSlice";
 
-const Mission = () => (
-  <div>
-    mission page
-  </div>
-);
+const Mission = () => {
+  const dispatch = useDispatch();
+  dispatch(getMissions());
+  const books = useSelector((store) => store.mission);
+  return <div>mission page</div>;
+};
 
 export default Mission;
