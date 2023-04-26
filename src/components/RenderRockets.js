8fafee8259ac27e2f6ec01edf83efb48dadd5a57
mohammad mohsen <mohsenhaidari0766@gmail.com>
2023-04-26 18:43:59 +0430

    events added to the rokect prototype

--- src/components/RenderRockets.js
+++ src/components/RenderRockets.js
@@ -35,6 +35,8 @@ const RenderRockets = () => {
         <Rocket
           key={rocket.id}
           rocket={rocket}
+          handleReserveClick={handleReserveClick}
+          onCancelClick={onCancelClick}
         />
       ))}
     </div>
