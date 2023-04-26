b27982c7ee1e966e2656a0aa4e8adf95ae767efd
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-04-26 11:44:54 +0200

    Update mission component

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,12 +1,3 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getMissions } from "../redux/mission/missionSlice";
-
-const Mission = () => {
-  const dispatch = useDispatch();
-  dispatch(getMissions());
-  const books = useSelector((store) => store.mission);
-  return <div>mission page</div>;
-};
+const Mission = () => <div>mission page</div>;
 
 export default Mission;
