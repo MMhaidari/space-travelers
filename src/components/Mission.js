40a46c8d92cda417c449b7a54058903084d760f7
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-04-26 14:05:52 +0200

    Display mission informations

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,3 +1,17 @@
-const Mission = () => <div>mission page</div>;
+import PropTypes from 'prop-types';
+
+const Mission = ({ mission }) => (
+  <tr>
+    <th>{mission.missionName}</th>
+    <th>{mission.description}</th>
+  </tr>
+);
+
+Mission.propTypes = {
+  mission: PropTypes.shape({
+    missionName: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Mission;
