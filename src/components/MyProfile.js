033d1d1530ef95a131f72c9693f81a11e98723cd
mohammad mohsen <mohsenhaidari0766@gmail.com>
2023-04-27 12:35:16 +0430

    profile section rocket funcationlity added

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,7 +2,9 @@ import { useSelector } from 'react-redux';
 import './MyProfile.css';
 
 const MyProfile = () => {
+  const rockets = useSelector((state) => state.rockets.rockets);
   const { missions } = useSelector((store) => store.missions);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
   return (
     <div className="profile-container">
       <section className="profile-section">
@@ -21,6 +23,22 @@ const MyProfile = () => {
       </section>
       <section className="profile-section">
         <h3 className="profile-section__header">My Rockets</h3>
+        <table className="profile-table">
+          <thead>
+            <tr>
+              {reservedRockets.length === 0 && (
+                <td>No Rockets Reserved</td>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            {reservedRockets.map((rockets) => (
+              <tr key={rockets.id}>
+                <td>{rockets.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </section>
     </div>
   );
